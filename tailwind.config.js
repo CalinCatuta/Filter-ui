04320a0adb60352ad8afebd64bc336cb62a5272b
CalinCatuta <calinut123@yahoo.co.uk>
2024-02-28 14:07:01 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./**/*.js"], // Process HTML and JavaScript files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
